16f97b51c0e5bbca789f5cc815944fe672f305de
lopezvalentin <97422036+lopezvalentin@users.noreply.github.com>
2022-01-10 00:58:24 -0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,11 +1,3 @@
-# Mawma this is garbage. I'm lying, it is not. It's just a silly program I'll be coding to practice all the new
-# stuff I'll be learning about this fascinating script named Python. As you may already know (lmao I'm writing this
-# to myself) I'm self-taught person. I'm self made bitches. An autodidact (that means that I love researching.
-# Investigating. Scrutinizing the inter web if you will, in order to acquire new valuable knowledge). I'm a very
-# knowledgeable person as you may or may not have noticed by the evident sapience exuded by the accoutrements
-# embellishing this delicate paragraph prefacing what could possibly be the proof of and precursor to a new Valentin
-# era. Enough dilly dallying. Ladies and gentlemen and everything in between... I present to you THE CODE.
-
 import pint
 name = 'Engel'
 first_name, last_name, full_name = 'Engelbert', 'Humperdinck', 'Engelbert Humperdinck'
